91104d855b4950ad3ea021684ae43521651512ed
gntoni <toni2332@gmail.com>
2022-02-11 16:50:29 +0900

    adds option to select gpu

--- experiments/scripts/train_virtual.sh
+++ experiments/scripts/train_virtual.sh
@@ -4,10 +4,10 @@ set -x
 set -e
 
 export PYTHONUNBUFFERED="True"
-export CUDA_VISIBLE_DEVICES=0
 
-python3 ./tools/train.py --dataset virtual\
-  --dataset_root /home/toni/Code/virtual_data_capture/Dataset\
+python ./tools/train.py --dataset virtual\
+  --dataset_root ~/Code/virtual_data_capture/Dataset\
   --refine_margin 0.024\
   --batch_size 8\
-  --exp_name "2_inc_dataset"
+  --exp_name "exp1"\
+  --gpu_idx 0
